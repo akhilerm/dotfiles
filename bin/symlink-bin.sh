fb11f13523eb51db1894933fc6cbdc2d85e7bbd9
Akhil Mohan <akhilerm@gmail.com>
2023-04-23 23:24:56 -0700

    modify binary symlink script
    modify symlink script to accept binaries also along with directories
    containing binaries
    
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- bin/symlink-bin.sh
+++ bin/symlink-bin.sh
@@ -7,15 +7,30 @@
 # $ symlink-bin build/bin/
 # $ symlink-bin build/bin/ prod
 
+make_symlink() {
+	binary=$1
+	binary_path=$2
+	suffix=$3
+	binary_name=$binary${suffix}
+	rm ~/bin/binary_name 2>/dev/null || true
+	ln -s "$PWD/${binary_path}/$binary" ~/bin/$binary_name
+}
+
+
 DEV_BINARY_PATH=$1
 DEV_SUFFIX="-dev"
 if ! [ -z "$2" ] && [ "$2" = "prod" ]; then
 	DEV_SUFFIX=""
 fi
 
+if [[ -f ${DEV_BINARY_PATH} ]]; then
+	if [[ -x ${DEV_BINARY_PATH} ]]; then
+		make_symlink ${DEV_BINARY_PATH} "" ${DEV_SUFFIX}
+		exit 0
+	fi
+	exit 1
+fi
+
 for binary in $(find "${DEV_BINARY_PATH}" -type f -executable -printf "%f\n");do
-	# remove existing devlinks before creating new
-	BINARY_NAME=$binary${DEV_SUFFIX}
-	rm ~/bin/$BINARY_NAME || true
-	ln -s "$PWD/$DEV_BINARY_PATH/$binary" ~/bin/$BINARY_NAME
+	make_symlink $binary ${DEV_BINARY_PATH} ${DEV_SUFFIX}
 done
