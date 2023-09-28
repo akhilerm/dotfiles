939b3cc88e6e114f9b7b7f1fac6c403980c41de0
Akhil Mohan <akhilerm@gmail.com>
2023-09-28 22:11:45 +0530

    add validation for bisect script
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- bin/do-bisect.sh
+++ bin/do-bisect.sh
@@ -4,6 +4,17 @@
 
 set -euxo pipefail
 
+
+print_usage() {
+	echo "do-bisect <REPO_ROOT> <GOOD_COMMIT> <BAD_COMMIT> <TEST_SCRIPT>"
+}
+
+if [ $# -ne 4 ]; then
+	echo "Error: Missing or incorrect number of arguments."
+	print_usage
+        exit 1
+fi
+
 REPO_ROOT="${1}"
 GOOD="${2}"
 BAD="${3}"
@@ -13,23 +24,40 @@ pushd "${PWD}"
 
 cd "${REPO_ROOT}"
 
-are_there_changes() {
-	return $( git diff-index --quiet HEAD --)
-}
-
-#TODO modify this area
-#if [[ ! "git diff-index --quiet HEAD --" ]]; then
+# TODO add validation to check there are no uncommited changes
+#are_there_changes() {
+#	return $( git diff-index --quiet HEAD -- )
+#}
+#
+## make sure that there are no uncommited changes
+#if are_there_changes ; then
 #	echo "Uncomitted changes present."
 #	exit 1
 #fi
+#
+# validate the script at $GOOD commit
+git checkout "${GOOD}"
+"${CHECKER_SCRIPT}" || true
+rc=$?
+if [[ $rc -ne 0 ]]; then
+	echo "test script fails at good commit"
+	exit 1
+fi
 
-# add validation to make sure the test script runs well in good and fails in  bad commits
+
+# validate the script at $BAD commit
+git checkout "${BAD}"
+"${CHECKER_SCRIPT}" || true
+rc=$?
+if [[ $rc -eq 0 ]]; then
+	echo "test script succeeded at bad commit"
+	exit 1
+fi
 
 git bisect start
 git bisect good "${GOOD}"
 git bisect bad "${BAD}"
 git bisect run "${CHECKER_SCRIPT}"
 git bisect reset
-
 
 exit 0
