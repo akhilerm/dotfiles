1c6876b49d3ffe9531cd14235b605464ea43f304
Akhil Mohan <akhil.mohan@broadcom.com>
2024-04-23 11:03:29 +0530

    make go switcher script work on all platforms
    Signed-off-by: Akhil Mohan <akhil.mohan@broadcom.com>

--- bin/switch-go.sh
+++ bin/switch-go.sh
@@ -5,14 +5,12 @@
 # Usage
 # switch-go.sh go1.18.3
 
-
 CURRENT_VERSION=$(basename $(readlink -f /usr/local/go))
 if [ -z $1 ]; then
 	echo "> $CURRENT_VERSION"
 	ls /usr/local/ | grep go | grep -vE "^go$|$CURRENT_VERSION" | awk '{print "  "$1}'
 	exit 0
 fi
-
 
 GO_VERSION=$1
 if [ -d "/usr/local/$GO_VERSION" ]; then
@@ -22,9 +20,16 @@ if [ -d "/usr/local/$GO_VERSION" ]; then
 	exit 0
 fi
 
+os=$(uname -s)
+os=$(echo $os | awk '{print tolower($0)}')
+
+arch=$(uname -m)
+if [[ $arch == "x86_64" ]]; then
+  arch="amd64"
+fi
 
 # download , extract and switch
-wget https://go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz
+wget https://go.dev/dl/${GO_VERSION}.$os-$arch.tar.gz
 rc=$?
 if [[ $rc -ne 0 ]]; then
 	echo "Not found. Version still at $CURRENT_VERSION"
@@ -34,13 +39,13 @@ fi
 # dirty hack to extract files , because I dont know how to extract go archive
 # to a particular directory
 sudo mkdir /usr/local/go-download
-sudo tar -C /usr/local/go-download -xzf ${GO_VERSION}.linux-amd64.tar.gz
+sudo tar -C /usr/local/go-download -xzf ${GO_VERSION}.$os-$arch.tar.gz
 sudo mv /usr/local/go-download/go /usr/local/${GO_VERSION}
 sudo rm /usr/local/go
 sudo ln -s /usr/local/${GO_VERSION} /usr/local/go
 sudo rm -r /usr/local/go-download
 
 #remove the downloads
-rm ${GO_VERSION}.linux-amd64.tar.gz
+rm ${GO_VERSION}.$os-$arch.tar.gz
 
 echo "Switched from $CURRENT_VERSION to $GO_VERSION"
