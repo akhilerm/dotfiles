c2a5dd326b595f7bf6d5c456efa668d7ef5223b3
Akhil Mohan <akhil.mohan@broadcom.com>
2025-07-07 14:38:38 +0530

    add script to disable intellij conflicting macOS keybinding
    Ref: https://gist.github.com/theodson/b4282a3b6e54091db4d52a4c3c10ad25 for original
    Signed-off-by: Akhil Mohan <akhil.mohan@broadcom.com>

--- mac_setup.sh
+++ mac_setup.sh
@@ -88,6 +88,34 @@ ln -s ~/Work/dotfiles/configs/ghostty-config-mac "$HOME/Library/Application Supp
 #setup default text editor
 defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.4;}'
 
+# remove conflicting man page and intellij shortcut
+TEMP_SETTINGS_FILE=$(mktemp -t 'man-shortcuts-off.json')
+cat > $TEMP_SETTINGS_FILE <<EOF
+{
+  "NSServicesStatus": {
+    "com.apple.Terminal - Open man Page in Terminal - openManPage": {
+      "presentation_modes": {
+        "ContextMenu": false,
+        "ServicesMenu": false
+      },
+      "enabled_context_menu": false,
+      "enabled_services_menu": false
+    },
+    "com.apple.Terminal - Search man Page Index in Terminal - searchManPages": {
+      "presentation_modes": {
+        "ContextMenu": false,
+        "ServicesMenu": false
+      },
+      "enabled_context_menu": false,
+      "enabled_services_menu": false
+    }
+  }
+}
+EOF
+# Settings are stored in the pbs domain. Other settings in this domain will not be overwritten. I’ve included the settings to change in JSON for brevity. They are converted to XML (which `defaults import` expects) before being imported.
+plutil -convert xml1 -o - ${TEMP_SETTINGS_FILE} | defaults import pbs -
+rm ${TEMP_SETTINGS_FILE}
+
 #setup golang directories
 mkdir go
 mkdir ~/go/bin ~/go/pkg ~/go/src
